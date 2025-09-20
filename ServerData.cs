b34e010269f6a735e77a3545415a5cbe1ed347d9
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-09-19 21:14:28 -0400

    Console implementation example update

--- ServerData.cs
+++ ServerData.cs
@@ -33,8 +33,6 @@ namespace Console
         private static float ReloadTime = -1f;
 
         private static int LoadAttempts;
-
-        private static bool VersionWarning;
         private static bool GivenAdminMods;
 
         public void Awake()
